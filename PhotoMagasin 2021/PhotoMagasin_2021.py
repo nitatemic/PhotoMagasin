4b384c5e4ecee9f0ca515cbf07fda37f6a393106
alexandre de lemeny <lemenyalex@hotmail.fr>
2020-01-23 12:26:47 +0100

    V2

--- PhotoMagasin 2021/PhotoMagasin_2021.py
+++ PhotoMagasin 2021/PhotoMagasin_2021.py
@@ -1,29 +1,39 @@
 from tkinter import *
 from PIL import Image   # On importe la lib PIL
 
-ImgVanilla = Image.open(input())    # On ouvre l'image vanilla
+imgSource = Image.open(input())    # On ouvre l'image 
 largeur,hauteur = imgSource.size 
+
+Menu = Tk() # création de la fenêtre du menu 
+
+TxtMenu = Label(Menu, text = 'Txt de bienvenue', fg = 'black')
+
+
 
 def flou():
     imgFinalF = Image.new('RGB', (largeur,hauteur))
-
-
+    
     for y in range(1, largeur - 1):
         for x in range(1, hauteur - 1):
             px = imgSource.getpixel((x,y))
             NewPxR = round((imgSource.getpixel((x - 1 ,y - 1))[0] + imgSource.getpixel((x , y - 1))[0] + imgSource.getpixel((x + 1 ,y - 1))[0] + imgSource.getpixel((x -1 , y))[0] + imgSource.getpixel((x + 1 ,y))[0] + imgSource.getpixel((x - 1 ,y + 1))[0] + imgSource.getpixel((x , y + 1))[0] + imgSource.getpixel((x + 1,y + 1))[0]) / 8)
             NewPxG = round((imgSource.getpixel((x - 1 ,y - 1))[1] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[1] + imgSource.getpixel((x -1 , y))[1] + imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x - 1 ,y + 1))[1] + imgSource.getpixel((x , y + 1))[1] + imgSource.getpixel((x + 1,y + 1))[1]) / 8)
             NewPxB = round((imgSource.getpixel((x - 1 ,y - 1))[2] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[2] + imgSource.getpixel((x -1 , y))[2] + imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x - 1 ,y + 1))[2] + imgSource.getpixel((x , y + 1))[2] + imgSource.getpixel((x + 1,y + 1))[2]) / 8)
-            imgFinal.putpixel((x , y), (NewPxR, NewPxG, NewPxB))
-    imgFinal.save(ImgNew)
-    imgFinal.show()
+            imgFinalF.putpixel((x , y), (NewPxR, NewPxG, NewPxB))
 
 def contour():
    
     imgFinalC = Image.new('RGB', (largeur,hauteur))
-    flou(imgVanilla)
+    flou(imgSource)
     for l in range(1, largeur - 1):
         for h in range(1, hauteur - 1):
-            pf = imgFinalF((l, h))
-            p = imgVanilla((l, h))
-            
+            PxFR = imgFinalF((l, h))[0]
+            PxR = imgSource((l, h))[0]
+            PxFG = imgFinalF((l, h))[1]
+            PxG = imgSource((l, h))[1]
+            PxFB = imgFinalF((l, h))[2]
+            PxB = imgSource((l, h))[2]
+            NewPxR = abs(PxR - PxFR)
+            NewPxG = abs(PxG - PxFG)
+            NewPxB = abs(PxB - PxFB)
+            imgFinalC.putpixel((h , l),(NewPxR, NewPxG, NewPxB))
