8f0086d8713c12f2c95ceebeee58878679779867
alexandre de lemeny <lemenyalex@hotmail.fr>
2020-01-30 12:27:58 +0100

    V2

--- PhotoMagasin 2021/PhotoMagasin_2021.py
+++ PhotoMagasin 2021/PhotoMagasin_2021.py
@@ -1,6 +1,13 @@
 from tkinter import *
 from PIL import Image   # On importe la lib PIL
 
+Menu = Tk() # création de la fenêtre du menu
+TxtMenu = Label(Menu, text = 'Txt de bienvenue', fg = 'black')
+TxtMenu.pack()
+LienImg = StringVar()
+Lien = Entry(Menu,text ="Lien de la photo", textvariable = LienImg)
+Lien.pack()
+
 imgSource = Image.open(input())    # On ouvre l'image 
 largeur,hauteur = imgSource.size 
 
@@ -16,38 +23,41 @@ def flou():     # Effet de flou
             NewPxG = round((imgSource.getpixel((x - 1 ,y - 1))[1] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[1] + imgSource.getpixel((x -1 , y))[1] + imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x - 1 ,y + 1))[1] + imgSource.getpixel((x , y + 1))[1] + imgSource.getpixel((x + 1,y + 1))[1]) / 8)
             NewPxB = round((imgSource.getpixel((x - 1 ,y - 1))[2] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[2] + imgSource.getpixel((x -1 , y))[2] + imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x - 1 ,y + 1))[2] + imgSource.getpixel((x , y + 1))[2] + imgSource.getpixel((x + 1,y + 1))[2]) / 8)
             imgFinalF.putpixel((x , y), (NewPxR, NewPxG, NewPxB))
+    imgFinalF.save("imgFlou.png")
 
-def contour():  # Effet de detection des contour
+#def contour():  # Effet de detection des contour
    
-    imgFinalC = Image.new('RGB', (largeur,hauteur))
-    flou()
-    for l in range(1, largeur - 1):
-        for h in range(1, hauteur - 1):
-            PxFR = imgFinalF((l, h))[0]
-            PxR = imgSource((l, h))[0]
-            PxFG = imgFinalF((l, h))[1]
-            PxG = imgSource((l, h))[1]
-            PxFB = imgFinalF((l, h))[2]
-            PxB = imgSource((l, h))[2]
-            NewPxR = abs(PxR - PxFR)
-            NewPxG = abs(PxG - PxFG)
-            NewPxB = abs(PxB - PxFB)
-            imgFinalC.putpixel((h , l),(NewPxR, NewPxG, NewPxB))
+#    imgFinalC = Image.new('RGB', (largeur,hauteur))
+#    flou()
+#    imgFinalF = Image.open("imgFlou.png")
+#    for l in range(1, largeur - 1):
+#        for h in range(1, hauteur - 1):
+#            PxFR = imgFinalF((l, h))[0]
+#            PxR = imgSource((l, h))[0]
+#            PxFG = imgFinalF((l, h))[1]
+#            PxG = imgSource((l, h))[1]
+#            PxFB = imgFinalF((l, h))[2]
+#            PxB = imgSource((l, h))[2]
+#            NewPxR = abs(PxR - PxFR)
+#            NewPxG = abs(PxG - PxFG)
+#            NewPxB = abs(PxB - PxFB)
+#            imgFinalC.putpixel((h , l),(NewPxR, NewPxG, NewPxB))
 
-def relief():   # Effet de relief
-    imgFinalR = Image.new('RGB', (largeur, hauteur))
+#def relief():   # Effet de relief
+#    imgFinalR = Image.new('RGB', (largeur, hauteur))
 
-    for l in range(1, largeur - 1):
-        for h in range(1, hauteur - 1):
-            px = imgSource.getpixel((x,y))
+#    for l in range(1, largeur - 1):
+#        for h in range(1, hauteur - 1):
+#            px = imgSource.getpixel((x,y))
 
 
 
-Menu = Tk() # création de la fenêtre du menu 
 
-TxtMenu = Label(Menu, text = 'Txt de bienvenue', fg = 'black')
-TxtMenu.pack()
-BtCtr = Button(Menu, text = 'Lancer contour', command = contour())
+BtFl = Button(Menu, text = 'Flouter la photo', command = flou())
+BtFl.pack()
+BtCtr = Button(Menu, text = 'Mettre en avant les contours de la photo', command = contour())
 BtCtr.pack()
-Lien = Entry(Menu,text ="Lien de la photo",textvariable = imgSource)
+BtRlf = Button(Menu, text = 'Ajouter des reliefs à la photo', command = relief())
+BtRlf.pack()
+
 Menu.mainloop()
