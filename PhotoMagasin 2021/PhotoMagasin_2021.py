9c8f58848e423fb1a6b17a4b5e9bb71790d7cc9f
Alexandre De Lemeny <lemenyalex@users.noreply.github.com>
2020-02-04 15:30:25 +0100

    Bouton Flou + select image + select save

--- PhotoMagasin 2021/PhotoMagasin_2021.py
+++ PhotoMagasin 2021/PhotoMagasin_2021.py
@@ -1,21 +1,22 @@
-from tkinter import filedialog
 from tkinter import *
+from tkinter import filedialog
+
+
 from PIL import Image   # On importe la lib PIL
 
 Menu = Tk() # création de la fenêtre du menu
-TxtMenu = Label(Menu, text = 'Txt de bienvenue', fg = 'black')
+Menu.geometry("800x100+400+400")
+Menu.title('Bienvenue sur PhotoMagasin 2021 !')
+Lien = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Fichier PNG","*.png"),))
+TxtMenu = Label(Menu, text = "Bienvenue sur Photo Magasin 2021, vous pouvez désormais appliquer l'effet de votre choix sur votre photo.", fg = 'black')
 TxtMenu.pack()
-LienImg = StringVar()
-Menu.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Fichier PNG","*.png"),))
-
-imgSource = Image.open(input())    # On ouvre l'image 
+imgSource = Image.open(Lien)    # On ouvre l'image 
 largeur,hauteur = imgSource.size 
 
 
 
 def flou():     # Effet de flou
-    imgFinalF = Image.new('RGB', (largeur,hauteur))
-    
+    imgFinalF = Image.new('RGB', (largeur,hauteur))   
     for x in range(1, largeur - 1):
         for y in range(1, hauteur - 1):
             px = imgSource.getpixel((x,y))
@@ -23,25 +24,26 @@ def flou():     # Effet de flou
             NewPxG = round((imgSource.getpixel((x - 1 ,y - 1))[1] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[1] + imgSource.getpixel((x -1 , y))[1] + imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x - 1 ,y + 1))[1] + imgSource.getpixel((x , y + 1))[1] + imgSource.getpixel((x + 1,y + 1))[1]) / 8)
             NewPxB = round((imgSource.getpixel((x - 1 ,y - 1))[2] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[2] + imgSource.getpixel((x -1 , y))[2] + imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x - 1 ,y + 1))[2] + imgSource.getpixel((x , y + 1))[2] + imgSource.getpixel((x + 1,y + 1))[2]) / 8)
             imgFinalF.putpixel((x , y), (NewPxR, NewPxG, NewPxB))
-    imgFinalF.save("imgFlou.png")
+    SaveLocaF = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png')
+    imgFinalF.save(SaveLocaF)
 
-#def contour():  # Effet de detection des contour
+def contour():  # Effet de detection des contour
    
-#    imgFinalC = Image.new('RGB', (largeur,hauteur))
-#    flou()
-#    imgFinalF = Image.open("imgFlou.png")
-#    for l in range(1, largeur - 1):
-#        for h in range(1, hauteur - 1):
-#            PxFR = imgFinalF((l, h))[0]
-#            PxR = imgSource((l, h))[0]
-#            PxFG = imgFinalF((l, h))[1]
-#            PxG = imgSource((l, h))[1]
-#            PxFB = imgFinalF((l, h))[2]
-#            PxB = imgSource((l, h))[2]
-#            NewPxR = abs(PxR - PxFR)
-#            NewPxG = abs(PxG - PxFG)
-#            NewPxB = abs(PxB - PxFB)
-#            imgFinalC.putpixel((h , l),(NewPxR, NewPxG, NewPxB))
+    imgFinalC = Image.new('RGB', (largeur,hauteur))
+    flou()
+    imgFinalF = Image.open("imgFlou.png")
+    for l in range(1, largeur - 1):
+        for h in range(1, hauteur - 1):
+            PxFR = imgFinalF((l, h))[0]
+            PxR = imgSource((l, h))[0]
+            PxFG = imgFinalF((l, h))[1]
+            PxG = imgSource((l, h))[1]
+            PxFB = imgFinalF((l, h))[2]
+            PxB = imgSource((l, h))[2]
+            NewPxR = abs(PxR - PxFR)
+            NewPxG = abs(PxG - PxFG)
+            NewPxB = abs(PxB - PxFB)
+            imgFinalC.putpixel((h , l),(NewPxR, NewPxG, NewPxB))
 
 #def relief():   # Effet de relief
 #    imgFinalR = Image.new('RGB', (largeur, hauteur))
@@ -54,10 +56,10 @@ def flou():     # Effet de flou
 
 
 BtnFl = Button(Menu, text = 'Flouter la photo', command = flou)
-BtnFl.pack()
+BtnFl.pack(side = LEFT, padx = 10, pady = 5)
 BtnCtr = Button(Menu, text = 'Mettre en avant les contours de la photo', command = contour)
 BtnCtr.pack()
-BtnRlf = Button(Menu, text = 'Ajouter des reliefs à la photo', command = relief)
-BtnRlf.pack()
+# BtnRlf = Button(Menu, text = 'Ajouter des reliefs à la photo', command = relief)
+# BtnRlf.pack()
 
 Menu.mainloop()
