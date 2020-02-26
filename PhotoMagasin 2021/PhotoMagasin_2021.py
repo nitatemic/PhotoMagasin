5a90ff0c8a5f1b4db8127fb88b0050e2b436e218
Alexandre De Lemeny <lemenyalex@users.noreply.github.com>
2020-02-26 14:25:57 +0100

    Correction

--- PhotoMagasin 2021/PhotoMagasin_2021.py
+++ PhotoMagasin 2021/PhotoMagasin_2021.py
@@ -2,70 +2,81 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image   # On importe la lib PIL
 
-
-Menu = Tk() # création de la fenêtre du menu
-Menu.geometry("900x100+400+400")
-Menu.title('PhotoMagasin 2021')
-Lien = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Fichier PNG","*.png"),))
-TxtMenu = Label(Menu, text = "Bienvenue sur Photo Magasin 2021, vous pouvez désormais appliquer l'effet de votre choix sur votre photo.", fg = 'black')
-TxtMenu.pack()
-imgSource = Image.open(Lien)    # On ouvre l'image 
-largeur, hauteur = imgSource.size 
-
-def lien():
+def lien(): # Fonction pour changer la photo
+    global largeur
+    global hauteur
+    global imgSource
     Lien = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Fichier PNG","*.png"),))
     imgSource = Image.open(Lien)    # On ouvre l'image 
     largeur, hauteur = imgSource.size
+    
+Menu = Tk() # Création de la fenêtre du menu
+Menu.geometry("900x100+400+400")
+Menu.title('PhotoMagasin 2021')
+TxtMenu = Label(Menu, text = "Bienvenue sur PhotoMagasin 2021, vous pouvez désormais appliquer l'effet de votre choix sur votre photo.", fg = 'black')
+TxtMenu.pack()
+lien()
+
+
 
    
 def flou():     # Effet de flou
     imgFinalF = Image.new('RGB', (largeur, hauteur))   
-    for x in range(1, largeur - 1):
-        for y in range(1, hauteur - 1):
+    for x in range(1, largeur - 1): # On crée une boucle qui prend en charge le nombre de pixel horizontaux
+        for y in range(1, hauteur - 1): # idem mais pour le nbr de pixels verticaux
             px = imgSource.getpixel((x,y))
+            # Matrice convolution de pixel en 3*3
             NewPxRF = round((imgSource.getpixel((x - 1 ,y - 1))[0] + imgSource.getpixel((x , y - 1))[0] + imgSource.getpixel((x + 1 ,y - 1))[0] + imgSource.getpixel((x -1 , y))[0] + imgSource.getpixel((x + 1 ,y))[0] + imgSource.getpixel((x - 1 ,y + 1))[0] + imgSource.getpixel((x , y + 1))[0] + imgSource.getpixel((x + 1,y + 1))[0]) / 8)
             NewPxGF = round((imgSource.getpixel((x - 1 ,y - 1))[1] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[1] + imgSource.getpixel((x -1 , y))[1] + imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x - 1 ,y + 1))[1] + imgSource.getpixel((x , y + 1))[1] + imgSource.getpixel((x + 1,y + 1))[1]) / 8)
             NewPxBF = round((imgSource.getpixel((x - 1 ,y - 1))[2] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[2] + imgSource.getpixel((x -1 , y))[2] + imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x - 1 ,y + 1))[2] + imgSource.getpixel((x , y + 1))[2] + imgSource.getpixel((x + 1,y + 1))[2]) / 8)
-            imgFinalF.putpixel((x , y), (NewPxRF, NewPxGF, NewPxBF))
-    SaveLocaF = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png')
+            imgFinalF.putpixel((x , y), (NewPxRF, NewPxGF, NewPxBF)) # Création de la nouvelle image
+    SaveLocaF = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png') # Explorateur pour choisir ou sauvegarder la photo
     imgFinalF.save(SaveLocaF)
+    imgFinalF.show() # On montre l'image modifiée
+    
 
 def contour():  # Effet de detection des contour
     imgFinalC = Image.new('RGB', (largeur, hauteur))
-    for x in range(1, largeur - 1):
-        for y in range(1, hauteur - 1):
+    for x in range(1, largeur - 1): # On crée une boucle qui prend en charge le nombre de pixel horizontaux
+        for y in range(1, hauteur - 1): # idem mais pour le nbr de pixels verticaux
             px = imgSource.getpixel((x,y))
+            # Matrice convolution de pixel en 3*3
             NewPxRC = imgSource.getpixel((x, y + 1))[0] + imgSource.getpixel((x , y - 1))[0] + imgSource.getpixel((x - 1 , y))[0] + imgSource.getpixel((x + 1 ,y))[0] + imgSource.getpixel((x , y))[0] * (-4)
             NewPxGC = imgSource.getpixel((x, y + 1))[1] + imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x - 1 , y))[1] + imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x , y))[1] * (-4) 
             NewPxBC = imgSource.getpixel((x, y + 1))[2] + imgSource.getpixel((x , y - 1))[2] + imgSource.getpixel((x - 1 , y))[2] + imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x , y))[2] * (-4) 
-            imgFinalC.putpixel((x , y), (NewPxRC, NewPxGC, NewPxBC))
-    SaveLocaC = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png')
+            imgFinalC.putpixel((x , y), (NewPxRC, NewPxGC, NewPxBC)) # Création de la nouvelle image
+    SaveLocaC = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png') # Explorateur pour choisir ou sauvegarder la photo
     imgFinalC.save(SaveLocaC)
+    imgFinalC.show() # On montre l'image modifiée
 
 
 def relief():   # Effet de relief
     imgFinalR = Image.new('RGB', (largeur, hauteur))
-    for x in range(1, largeur - 1):
-        for y in range(1, hauteur - 1):
+    for x in range(1, largeur - 1): # On crée une boucle qui prend en charge le nombre de pixel horizontaux
+        for y in range(1, hauteur - 1): # idem mais pour le nbr de pixels verticaux
             px = imgSource.getpixel((x,y))
+            # Matrice convolution de pixel en 3*3
             NewPxRF = imgSource.getpixel((x , y - 1))[0] + imgSource.getpixel((x + 1 ,y - 1))[0] * (2) - imgSource.getpixel((x -1 , y))[0] + imgSource.getpixel((x + 1 ,y))[0] + imgSource.getpixel((x ,y))[0] + imgSource.getpixel((x - 1 ,y + 1))[0] *(-2) - imgSource.getpixel((x , y + 1))[0]
             NewPxGF = imgSource.getpixel((x , y - 1))[1] + imgSource.getpixel((x + 1 ,y - 1))[1] * (2) - imgSource.getpixel((x -1 , y))[1] + imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x ,y))[1] + imgSource.getpixel((x - 1 ,y + 1))[1] *(-2) - imgSource.getpixel((x , y + 1))[1]
             NewPxBF = imgSource.getpixel((x , y - 1))[2] + imgSource.getpixel((x + 1 ,y - 1))[2] * (2) - imgSource.getpixel((x -1 , y))[2] + imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x ,y))[2] + imgSource.getpixel((x - 1 ,y + 1))[2] *(-2) - imgSource.getpixel((x , y + 1))[2]
-            imgFinalR.putpixel((x , y), (NewPxRF, NewPxGF, NewPxBF))
-    SaveLocaR = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png')
+            imgFinalR.putpixel((x , y), (NewPxRF, NewPxGF, NewPxBF)) # Création de la nouvelle image
+    SaveLocaR = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png') # Explorateur pour choisir ou sauvegarder la photo
     imgFinalR.save(SaveLocaR)
+    imgFinalR.show() # On montre l'image modifiée
 
 def contraste():   # Effet de contraste
     imgFinalCO = Image.new('RGB', (largeur, hauteur))
-    for x in range(1, largeur - 1):
-        for y in range(1, hauteur - 1):
+    for x in range(1, largeur - 1): # On crée une boucle qui prend en charge le nombre de pixel horizontaux
+        for y in range(1, hauteur - 1): # idem mais pour le nbr de pixels verticaux
             px = imgSource.getpixel((x,y))
+            # Matrice convolution de pixel en 3*3
             NewPxRCO = -imgSource.getpixel((x , y - 1))[0] - imgSource.getpixel((x - 1 , y))[0] - imgSource.getpixel((x + 1 ,y))[0] + imgSource.getpixel((x ,y))[0] * 5 - imgSource.getpixel((x , y + 1))[0]
             NewPxGCO = -imgSource.getpixel((x , y - 1))[1] - imgSource.getpixel((x - 1 , y))[1] - imgSource.getpixel((x + 1 ,y))[1] + imgSource.getpixel((x ,y))[1] * 5 - imgSource.getpixel((x , y + 1))[1]
             NewPxBCO = -imgSource.getpixel((x , y - 1))[2] - imgSource.getpixel((x - 1 , y))[2] - imgSource.getpixel((x + 1 ,y))[2] + imgSource.getpixel((x ,y))[2] * 5 - imgSource.getpixel((x , y + 1))[2]
-            imgFinalCO.putpixel((x , y), (NewPxRCO, NewPxGCO, NewPxBCO))
-    SaveLocaCO = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png')
+            imgFinalCO.putpixel((x , y), (NewPxRCO, NewPxGCO, NewPxBCO)) # Création de la nouvelle image
+    SaveLocaCO = filedialog.asksaveasfilename(initialdir = "/",title = "Selectionner un emplacement de sauvegarde",filetypes = (("Fichier PNG","*.png"),), defaultextension='.png') # Explorateur pour choisir ou sauvegarder la photo
     imgFinalCO.save(SaveLocaCO)
+    imgFinalCO.show() # On montre l'image modifiée
 
 BtnFl = Button(Menu, text = 'Flouter la photo', command = flou)
 BtnFl.pack(side = LEFT, padx = 20, pady = 5)
@@ -79,3 +90,6 @@ BtnCtrst = Button(Menu, text = 'Changer le contraste de la photo', command = con
 BtnCtrst.pack(side = LEFT, padx = 10,pady = 5)
 Menu.resizable(width=False, height=False)
 Menu.mainloop()
+
+#  Progrmamme terminée le 06/02/2020 à 11:16
+# © Lemenyalex et Matthieu Azzoun Minet 
