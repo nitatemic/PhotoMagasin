cac5fd755eed0c85cba9f0f798cca153a6694581
Alexandre de Lemeny-Makedone <lemenyalex@protonmail.com>
2021-04-16 21:35:51 +0200

    Delete Visual Studio files

--- PhotoMagasin 2021/Contours.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image   # On importe la lib PIL
-
-
-imgSource = Image.open("chien.png")    # On ouvre l'image fruits.png
-largeur,hauteur = imgSource.size        # On recupere la taille de l'image (En pixel)
-imgFinal = Image.new('RGB', (largeur,hauteur))
